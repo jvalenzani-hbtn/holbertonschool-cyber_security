157db42053698e40fed6e4ad95291118513222b0
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2024-11-18 17:08:37 -0300

    Update 0-attack_ip.sh

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 IPREGEXP="[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+"
-grep -o ${IPREGEXP} $1 | sort | uniq -c | sort -br | head -1 | tr -s ' ' | cut -d ' ' -f 3
+grep -o ${IPREGEXP} logs.txt | sort | uniq -c | sort -br | head -1 | tr -s ' ' | cut -d ' ' -f 3
